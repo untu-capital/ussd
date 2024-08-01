29eca7496bc267540eeca93c9b6342dda74d6875
Administrator <panashe.rutimhu@untucapital.co.zw>
2024-08-01 13:05:02 +0200

    Fixing deployment errors

--- scripts/ValidateService.ps1
+++ scripts/ValidateService.ps1
@@ -1,14 +1,29 @@
-
 Write-Output "Running ValidateService script"
 
 $jarFilePath = "C:\deployments\ussd\ussd.jar"
+$maxRetries = 5
+$delay = 10
+$found = $false
 
-$javaProcess = Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like "*$jarFilePath*" }
+for ($i = 1; $i -le $maxRetries; $i++) {
+    Write-Output "Attempt $i: Checking for Java process running $jarFilePath..."
 
-if ($javaProcess) {
-    Write-Output "Java process running $jarFilePath found."
-    exit 0
-} else {
-    Write-Output "Java process running $jarFilePath is not found."
+    $javaProcess = Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like "*$jarFilePath*" }
+
+    if ($javaProcess) {
+        Write-Output "Java process running $jarFilePath found."
+        $found = $true
+        break
+    } else {
+        Write-Output "Java process running $jarFilePath is not found. Retrying in $delay seconds..."
+        Start-Sleep -Seconds $delay
+    }
+}
+
+if (-not $found) {
+    Write-Output "Java process running $jarFilePath is not found after $maxRetries attempts."
     exit 1
+} else {
+    Write-Output "Java process running $jarFilePath successfully found."
+    exit 0
 }
