38ae9c08026b07513eaa6d205197520f679f25c4
Administrator <panashe.rutimhu@untucapital.co.zw>
2024-08-01 16:04:53 +0200

    Fixing deployment errors

--- src/main/java/com/example/ussd1/Ussd1Application.java
+++ src/main/java/com/example/ussd1/Ussd1Application.java
@@ -1,5 +1,6 @@
 package com.example.ussd1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,10 +10,12 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@Slf4j
 public class Ussd1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Ussd1Application.class, args);
+        log.info("<<<<<<<<<<<<<<<APPLICATION STARTED>>>>>>>>>>>>>>>");
     }
 
     @Bean
