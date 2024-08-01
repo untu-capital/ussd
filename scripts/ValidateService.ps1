26a669fa0d6631bfe09a1ccfedaa56ab3e620b0d
Administrator <panashe.rutimhu@untucapital.co.zw>
2024-08-01 15:16:48 +0200

    Fixing deployment errors

--- scripts/ValidateService.ps1
+++ scripts/ValidateService.ps1
@@ -6,13 +6,15 @@ $delay = 10
 $found = $false
 
 for ($i = 1; $i -le $maxRetries; $i++) {
-    Write-Output "Attempt ${i}: Checking for Java process running ${jarFilePath}..."
+    Write-Output "Attempt ${i}: Checking for Java process running $jarFilePath..."
 
+    # Get Java processes
     $javaProcesses = Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like "*java*" }
 
+    # Check each Java process
     foreach ($process in $javaProcesses) {
-        if ($process.CommandLine -like "*-jar ${jarFilePath}*") {
-            Write-Output "Java process running ${jarFilePath} found."
+        if ($process.CommandLine -like "*-jar $jarFilePath*") {
+            Write-Output "Java process running $jarFilePath found."
             $found = $true
             break
         }
@@ -21,15 +23,15 @@ for ($i = 1; $i -le $maxRetries; $i++) {
     if ($found) {
         break
     } else {
-        Write-Output "Java process running ${jarFilePath} is not found. Retrying in ${delay} seconds..."
-        Start-Sleep -Seconds ${delay}
+        Write-Output "Java process running $jarFilePath is not found. Retrying in $delay seconds..."
+        Start-Sleep -Seconds $delay
     }
 }
 
 if (-not $found) {
-    Write-Output "Java process running ${jarFilePath} is not found after ${maxRetries} attempts."
+    Write-Output "Java process running $jarFilePath is not found after $maxRetries attempts."
     exit 1
 } else {
-    Write-Output "Java process running ${jarFilePath} successfully found."
+    Write-Output "Java process running $jarFilePath successfully found."
     exit 0
 }
